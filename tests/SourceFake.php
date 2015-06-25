3e0220a65ced4f2a76f9c10463e9a7e6f46e9cf1
lorenzop <lorenzo@poixson.com>
2015-06-25 18:29:56 -0400

    moved test json data from FakeJSON.php file into SourceFake class deleted FakeJSON.php file

--- tests/SourceFake.php
+++ tests/SourceFake.php
@@ -14,7 +14,146 @@ class SourceFake extends \pxn\phpJenkins\Source {
 
 
 	protected function wget() {
-		return FakeJSON::viewAll();
+		return <<<EOF
+{
+  "description" : null,
+  "jobs" : [
+    {
+      "actions" : [],
+      "description" : "",
+      "displayName" : "TestJobA",
+      "displayNameOrNull" : "TestJobA",
+      "name" : "test-job-A",
+      "url" : "http://127.0.0.1:8080/job/test-job-A/",
+      "buildable" : true,
+      "builds" : [
+        {
+          "number" : 2,
+          "url" : "http://127.0.0.1:8080/job/test-job-A/2/"
+        },
+        {
+          "number" : 1,
+          "url" : "http://127.0.0.1:8080/job/test-job-A/1/"
+        }
+      ],
+      "color" : "blue",
+      "firstBuild" : {
+        "number" : 1,
+        "url" : "http://127.0.0.1:8080/job/test-job-A/1/"
+      },
+      "healthReport" : [
+        {
+          "description" : "Build stability: No recent builds failed.",
+          "iconClassName" : "icon-health-80plus",
+          "iconUrl" : "health-80plus.png",
+          "score" : 100
+        }
+      ],
+      "inQueue" : false,
+      "keepDependencies" : false,
+      "lastBuild" : {
+        "number" : 2,
+        "url" : "http://127.0.0.1:8080/job/test-job-A/2/"
+      },
+      "lastCompletedBuild" : {
+        "number" : 2,
+        "url" : "http://127.0.0.1:8080/job/test-job-A/2/"
+      },
+      "lastFailedBuild" : null,
+      "lastStableBuild" : {
+        "number" : 2,
+        "url" : "http://127.0.0.1:8080/job/test-job-A/2/"
+      },
+      "lastSuccessfulBuild" : {
+        "number" : 2,
+        "url" : "http://127.0.0.1:8080/job/test-job-A/2/"
+      },
+      "lastUnstableBuild" : null,
+      "lastUnsuccessfulBuild" : null,
+      "nextBuildNumber" : 3,
+      "property" : [],
+      "queueItem" : null,
+      "concurrentBuild" : false,
+      "downstreamProjects" : [],
+      "scm" : {},
+      "upstreamProjects" : []
+    },
+    {
+      "actions" : [],
+      "description" : "",
+      "displayName" : "test-job-B",
+      "displayNameOrNull" : null,
+      "name" : "test-job-B",
+      "url" : "http://127.0.0.1:8080/job/test-job-B/",
+      "buildable" : true,
+      "builds" : [
+        {
+          "number" : 3,
+          "url" : "http://127.0.0.1:8080/job/test-job-B/3/"
+        },
+        {
+          "number" : 2,
+          "url" : "http://127.0.0.1:8080/job/test-job-B/2/"
+        },
+        {
+          "number" : 1,
+          "url" : "http://127.0.0.1:8080/job/test-job-B/1/"
+        }
+      ],
+      "color" : "blue",
+      "firstBuild" : {
+        "number" : 1,
+        "url" : "http://127.0.0.1:8080/job/test-job-B/1/"
+      },
+      "healthReport" : [
+        {
+          "description" : "Build stability: No recent builds failed.",
+          "iconClassName" : "icon-health-80plus",
+          "iconUrl" : "health-80plus.png",
+          "score" : 100
+        }
+      ],
+      "inQueue" : false,
+      "keepDependencies" : false,
+      "lastBuild" : {
+        "number" : 3,
+        "url" : "http://127.0.0.1:8080/job/test-job-B/3/"
+      },
+      "lastCompletedBuild" : {
+        "number" : 3,
+        "url" : "http://127.0.0.1:8080/job/test-job-B/3/"
+      },
+      "lastFailedBuild" : {
+        "number" : 2,
+        "url" : "http://127.0.0.1:8080/job/test-job-B/2/"
+      },
+      "lastStableBuild" : {
+        "number" : 3,
+        "url" : "http://127.0.0.1:8080/job/test-job-B/3/"
+      },
+      "lastSuccessfulBuild" : {
+        "number" : 3,
+        "url" : "http://127.0.0.1:8080/job/test-job-B/3/"
+      },
+      "lastUnstableBuild" : null,
+      "lastUnsuccessfulBuild" : {
+        "number" : 2,
+        "url" : "http://127.0.0.1:8080/job/test-job-B/2/"
+      },
+      "nextBuildNumber" : 4,
+      "property" : [],
+      "queueItem" : null,
+      "concurrentBuild" : false,
+      "downstreamProjects" : [],
+      "scm" : {},
+      "upstreamProjects" : []
+    }
+  ],
+  "name" : "All",
+  "property" : [],
+  "url" : "http://127.0.0.1:8080/"
+}
+EOF;
 	}
 
 
